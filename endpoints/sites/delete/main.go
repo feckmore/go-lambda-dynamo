fbadbcb77dea6db8fc6a0180ec4253c6d647d2ab
David Schultz <dschultz@arthrex.com>
2019-04-12 12:26:36 -0500

    update delete endpoint

--- endpoints/sites/delete/main.go
+++ endpoints/sites/delete/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
+	"os"
+	"strings"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -12,25 +14,84 @@ import (
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
-var db *dynamodb.DynamoDB
-
 type Response events.APIGatewayProxyResponse
 type Request events.APIGatewayProxyRequest
+type SiteStatus int
+
+const (
+	Unpublished SiteStatus = iota
+	Published
+)
+
+// Site defines the fields of the site model
+type Site struct {
+	ID           string     `json:"id"`
+	Version      string     `json:"version"`
+	Path         string     `json:"path"`
+	Type         string     `json:"type"`
+	Status       SiteStatus `json:"status,omitempty"`
+	Name         *string    `json:"name,omitempty"`
+	Description  *string    `json:"description,omitempty"`
+	Keywords     *string    `json:"keywords,omitempty"`
+	URL          *string    `json:"url,omitempty"`
+	TagManagerID *string    `json:"tagManagerId,omitempty"`
+	CardImageURL *string    `json:"cardImageUrl,omitempty"`
+	CreatedAt    time.Time  `json:"createdAt,omitempty"`
+	UpdatedAt    time.Time  `json:"updatedAt,omitempty"`
+}
+
+var db *dynamodb.DynamoDB
+var region, stage, table string
+var currentTime time.Time
+
+func init() {
+	// Enable line numbers in log output, but remove date/time
+	log.SetFlags(log.Llongfile)
+
+	region = strings.TrimSpace(os.Getenv("AWS_REGION"))
+	stage = os.Getenv("STAGE")
+	table = os.Getenv("TABLE_NAME")
+
+	log.Println("AWS_REGION:", region)
+	log.Println("STAGE:", stage)
+	log.Println("TABLE_NAME:", table)
+
+	// TODO: validate env vars
+}
+
+// main starts the session, news up the db & invokes lambda handler
+func main() {
+	session, err := session.NewSession(&aws.Config{Region: aws.String(region)})
+	if err != nil {
+		log.Println("Failed to connect to AWS:", err)
+	} else {
+		db = dynamodb.New(session)
+	}
+
+	lambda.Start(Handler)
+}
 
 // Handler is our lambda handler invoked by the `lambda.Start` function call in main()
-func Handler(ctx context.Context, req Request) (Response, error) {
-	id := aws.String(req.PathParameters["id"])
-	key := map[string]*dynamodb.AttributeValue{"id": {S: id}}
+func Handler(ctx context.Context, request Request) (Response, error) {
+	id := aws.String(request.PathParameters["id"])
+	version := aws.String(request.QueryStringParameters["version"])
+
+	// TODO: also option to delete all versions?
+
+	key := map[string]*dynamodb.AttributeValue{
+		"id":      {S: id},
+		"version": {S: version},
+	}
 
 	_, err := db.DeleteItem(&dynamodb.DeleteItemInput{
 		Key:       key,
-		TableName: aws.String("todos"),
+		TableName: aws.String(table),
 	})
 	if err != nil {
 		return Response{StatusCode: 500}, err // TODO: decide what's the correct status
 	}
 
-	resp := Response{
+	response := Response{
 		StatusCode: 200,
 		Headers: map[string]string{
 			"Access-Control-Allow-Origin":      "*",
@@ -38,16 +99,5 @@ func Handler(ctx context.Context, req Request) (Response, error) {
 		},
 	}
 
-	return resp, nil
-}
-
-func main() {
-	session, err := session.NewSession(&aws.Config{Region: aws.String("us-east-1")}) // TODO: REMOVE HARD-CODED REGION string
-	if err != nil {
-		log.Println(fmt.Sprintf("Failed to connect to AWS: %s", err.Error()))
-	} else {
-		db = dynamodb.New(session)
-	}
-
-	lambda.Start(Handler)
+	return response, nil
 }
