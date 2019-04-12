245fd65be46a556b2f957e3611c514a718533798
David Schultz <dschultz@arthrex.com>
2019-04-12 12:07:58 -0500

    remove commented out code

--- endpoints/sites/get/main.go
+++ endpoints/sites/get/main.go
@@ -1,76 +1,3 @@
-// package main
-
-// import (
-// 	"bytes"
-// 	"context"
-// 	"encoding/json"
-// 	"fmt"
-// 	"log"
-
-// 	"github.com/aws/aws-lambda-go/events"
-// 	"github.com/aws/aws-lambda-go/lambda"
-// 	"github.com/aws/aws-sdk-go/aws"
-// 	"github.com/aws/aws-sdk-go/aws/session"
-// 	"github.com/aws/aws-sdk-go/service/dynamodb"
-// 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
-// )
-
-// var db *dynamodb.DynamoDB
-
-// type Response events.APIGatewayProxyResponse
-// type Request events.APIGatewayProxyRequest
-
-// // Handler is our lambda handler invoked by the `lambda.Start` function call in main()
-// func Handler(ctx context.Context, req Request) (Response, error) {
-// 	id := aws.String(req.PathParameters["id"])
-// 	key := map[string]*dynamodb.AttributeValue{"id": {S: id}}
-
-// 	result, err := db.GetItem(&dynamodb.GetItemInput{
-// 		Key:       key,
-// 		TableName: aws.String("todos"),
-// 	})
-
-// 	if err != nil {
-// 		return Response{StatusCode: 500}, err // TODO: decide what's the correct status
-// 	}
-
-// 	var castTo interface{} // TODO: Consider replacing interface{}?
-// 	if err := dynamodbattribute.UnmarshalMap(result.Item, &castTo); err != nil {
-// 		return Response{StatusCode: 404}, err // TODO: decide what's the correct status
-// 	}
-// 	body, err := json.Marshal(castTo)
-// 	if err != nil {
-// 		return Response{StatusCode: 404}, err // TODO: decide what's the correct status
-// 	}
-
-// 	var buf bytes.Buffer
-// 	json.HTMLEscape(&buf, body)
-
-// 	resp := Response{
-// 		StatusCode:      200,
-// 		IsBase64Encoded: false,
-// 		Body:            buf.String(),
-// 		Headers: map[string]string{
-// 			"Content-Type":                     "application/json",
-// 			"Access-Control-Allow-Origin":      "*",
-// 			"Access-Control-Allow-Credentials": "true",
-// 		},
-// 	}
-
-// 	return resp, nil
-// }
-
-// func main() {
-// 	session, err := session.NewSession(&aws.Config{Region: aws.String("us-east-1")}) // TODO: REMOVE HARD-CODED REGION string
-// 	if err != nil {
-// 		log.Println(fmt.Sprintf("Failed to connect to AWS: %s", err.Error()))
-// 	} else {
-// 		db = dynamodb.New(session)
-// 	}
-
-// 	lambda.Start(Handler)
-// }
-
 package main
 
 import (
@@ -153,27 +80,6 @@ func Handler(ctx context.Context, request Request) (Response, error) {
 
 	id := aws.String(request.PathParameters["id"])
 	version := aws.String(request.QueryStringParameters["version"])
-
-	// key := expression.Key("type").Equal(expression.Value("site"))
-	// builder := expression.NewBuilder().WithKeyCondition(key)
-	// expr, err := builder.Build()
-	// if err != nil {
-	// 	log.Println("Error building dynamodb expression")
-	// 	return Response{StatusCode: http.StatusInternalServerError}, err
-	// }
-
-	// queryInput := dynamodb.QueryInput{
-	// 	KeyConditionExpression:    expr.KeyCondition(),
-	// 	ExpressionAttributeNames:  expr.Names(),
-	// 	ExpressionAttributeValues: expr.Values(),
-	// 	IndexName:                 aws.String("type-path-index"),
-	// 	TableName:                 aws.String(table),
-	// }
-	// results, err := db.Query(&queryInput)
-	// if err != nil {
-	// 	log.Println("Error querying sites in dynamodb")
-	// 	return Response{StatusCode: http.StatusInternalServerError}, err
-	// }
 
 	key := map[string]*dynamodb.AttributeValue{
 		"id":      {S: id},
